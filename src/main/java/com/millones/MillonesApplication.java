fba85973813606f226d58280d3560c54b47955c5
Mauricio herreram <ofmami7@gmail.com>
2022-09-21 14:52:48 -0500

    first commit

---
+++ src/main/java/com/millones/MillonesApplication.java
@@ -0,0 +1,13 @@
+package com.millones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MillonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/millones/MillonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

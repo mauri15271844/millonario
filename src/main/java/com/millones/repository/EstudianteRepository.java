ef519a1d618f0f409d0b96d93a46532e25df08a0
Mauricio herreram <ofmami7@gmail.com>
2022-09-21 15:11:47 -0500

    second commit

--- src/main/java/com/millones/repository/EstudianteRepository.java
+++ src/main/java/com/millones/repository/EstudianteRepository.java
@@ -2,6 +2,9 @@ package com.millones.repository;
 
 import com.millones.entity.Estudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
 }

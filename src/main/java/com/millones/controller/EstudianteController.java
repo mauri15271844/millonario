42d463993f3ca5172892088871eaeb4f247b97ee
Mauricio herreram <ofmami7@gmail.com>
2022-09-22 13:35:10 -0500

    commit interfaces pendientes

--- src/main/java/com/millones/controller/EstudianteController.java
+++ src/main/java/com/millones/controller/EstudianteController.java
@@ -4,9 +4,20 @@ package com.millones.controller;
 import com.millones.service.EstudianteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EstudianteController {
     @Autowired
     private EstudianteService service;
+
+    @GetMapping("/")
+    public String soyMillonario(){
+        return "index";
+    }
+
+    @GetMapping("/new")
+    public String crear_millonarios(){
+        return "crear_millonarios";
+    }
 }
--- src/main/java/com/millones/service/EstudianteService.java
+++ src/main/java/com/millones/service/EstudianteService.java
@@ -1,15 +1,24 @@
 package com.millones.service;
 
+import com.millones.entity.Estudiante;
 import com.millones.repository.EstudianteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EstudianteService {
 
     @Autowired
-    private EstudianteRepository epository;
+    private EstudianteRepository repository;
 
+    public List<Estudiante> verEstudiantes(){
+        return repository.findAll();
+    }
 
+    public void crear_estudiante(Estudiante nuevo_estudiante){
+        repository.save(nuevo_estudiante);
+    }
 
 }
